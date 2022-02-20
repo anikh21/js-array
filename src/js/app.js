be6c66f6d857a01c5c4e7f3932215a0bef6f9bed
Anik Hossain <anik.wdev@gmail.com>
2022-02-20 21:30:00 +0600

    incomplete lesson

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,21 @@
 // line break function
-function lb(color = 'black', title){
-    if (!title) {
-        console.log(
-        "%c**************************************************",
-        `color:${color}`
+function lb(color = 'black', title, New = false) {
+  if (!title) {
+    console.log(
+      '%c**************************************************',
+      `color:${color}`
     );
-    }else{
-        console.log(
-            `%c************************* ${title} *************************`,
-            `color:${color}`
-        );
+  } else {
+    if (New) {
+      console.log(
+        `%c************************* ${title} *************************`,
+        `color:${color || 'green'};font-size: 20px`
+      );
+    } else {
+      console.log(
+        `%c************************* ${title} *************************`,
+        `color:${color}`
+      );
     }
+  }
 }
---
+++ src/js/array-2.js
@@ -0,0 +1,18 @@
+/**
+ * @Title: Update Array Elements and Fill Array
+ */
+
+lb(false, 'Update Array Elements and Fill Array', true);
+console.log('');
+
+// Array Fill Manual Way
+const arr1 = new Array(18);
+
+// Array Fill
+const arr2 = new Array(18);
+
+const names = ['Anik', 'Shakil', 'Hossain'];
+
+// console.log(names);
+
+// Fill Array and Update
