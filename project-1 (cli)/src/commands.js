555f029d7fca07496e2fa1d80e17496df9899cdf
Anik Hossain <anik.wdev@gmail.com>
2022-04-12 09:39:05 +0600

    js cli base application

---
+++ project-1 (cli)/src/commands.js
@@ -0,0 +1,15 @@
+const ADD = 'add';
+const UPDATE = 'update';
+const DONE = 'done';
+const NEXT = 'next';
+const FIND = 'find';
+const LIST = 'list';
+
+module.exports = {
+  ADD,
+  UPDATE,
+  DONE,
+  NEXT,
+  FIND,
+  LIST,
+};
